9368f752a28945a233f8ca1fbd5a9399e0398474
Juan Antonio Martin <juanantoniomartinb@gmail.com>
2020-05-09 14:05:37 +0200

    first commit

---
+++ components/layout/Header.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import Buscar from '../ui/Buscar'
+import Navegacion from './Navegacion'
+import Button from '../ui/Boton'
+
+import Link from 'next/link'
+import styled from '@emotion/styled'
+import { css } from '@emotion/core'
+
+const HeaderContainer = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width: 768px) {
+        display: flex;
+        justify-content: space-between;
+    }
+`
+
+const Logo = styled.p`
+    color: var(--naranja);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+`
+
+const Header = () => {
+
+    const usuario = false
+
+    return (
+        <header
+            css={css`
+                border-bottom: 2px solid var(--gris3);
+                padding: 1rem 0;
+                `}
+        >
+            <HeaderContainer>
+                <div
+                    css={css`
+                    display: flex;
+                    align-items: center;
+                `}
+                >
+                    <Link href="/">
+                        <Logo>P</Logo>
+                    </Link>
+
+                    <Buscar />
+
+                    <Navegacion />
+                </div>
+
+                <div
+                    css={css`
+                        display: flex;
+                        align-items: center;
+                    `}
+                >
+                    {usuario ? (
+                        <>
+                            <p
+                                css={css`
+                        margin-right: 2rem;
+                    `}
+                            >Hola: Juancho</p>
+                            <Button
+                                bgColor="true"
+                            >Cerrar Sesión</Button>
+                        </>
+                    ) : (
+                            <>
+                                <Link href="/login">
+                                    <Button
+                                        bgColor="true"
+                                    >Login</Button>
+                                </Link>
+                                <Link href="/crear-cuenta">
+                                    <Button>Crear Cuenta</Button>
+                                </Link>
+                            </>
+                        )}
+                </div>
+            </HeaderContainer>
+        </header>
+
+    )
+}
+
+export default Header
---
+++ pages/nuevo-producto.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/layout/Layout'
+
+
+const NuevoProducto = () => (
+    <div>
+        <Layout>
+            <h1>NuevoProducto</h1>
+
+        </Layout>
+
+    </div>
+)
+
+
+export default NuevoProducto
